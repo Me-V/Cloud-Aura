61d6b262985befebf79019f8c4c471756bc25e47
Me-V <gonemanarmy@gmail.com>
2024-10-16 22:51:33 +0530

    added loading percentage and the sticky upload button

--- components/uploadButton.tsx
+++ components/uploadButton.tsx
@@ -12,6 +12,10 @@ import {
     FormLabel,
     Input,
     Text,
+    CircularProgress,
+    CircularProgressLabel,
+    Flex,
+    Tooltip,
 } from "@chakra-ui/react";
 import { useMutation } from 'convex/react';
 import { api } from '@/convex/_generated/api';
@@ -39,6 +43,7 @@ const UploadButton = () => {
     const fileInputRef = useRef<HTMLInputElement>(null);
     const generateUploadUrl = useMutation(api.files.generateUploadUrl);
     const [isFormValid, setIsFormValid] = useState(false);
+    const [uploadProgress, setUploadProgress] = useState(0);
 
     const onOpen = () => setIsOpen(true);
     const onClose = () => {
@@ -72,34 +77,50 @@ const UploadButton = () => {
 
         const postUrl = await generateUploadUrl();
 
-        const result = await fetch(postUrl, {
-            method: "POST",
-            headers: { "Content-Type": fileInputRef.current!.files![0].type },
-            body: fileInputRef.current!.files![0],
-        });
+        const xhr = new XMLHttpRequest();
+        xhr.open("POST", postUrl);
+        xhr.setRequestHeader("Content-Type", fileInputRef.current!.files![0].type);
 
-        const { storageId } = await result.json();
-
-        if (result.ok) {
-
-            await createFiles({ name: fileName, orgId, fileId: storageId, type: fileInputRef.current!.files![0].type as FileType });
-            onClose();
-            setIsLoading(false);
-            setFileName('');
-            // setFileType('');
-            setError(null);
-            if (fileInputRef.current) {
-                fileInputRef.current.value = '';
+        xhr.upload.onprogress = (event) => {
+            if (event.lengthComputable) {
+                const percentComplete = (event.loaded / event.total) * 100;
+                setUploadProgress(percentComplete);
             }
+        };
 
-            toast({
-                title: "File uploaded successfully",
-                status: "success",
-                duration: 2000,
-                isClosable: true,
-                position: "bottom-right"
-            });
-        } else {
+        xhr.onload = async () => {
+            if (xhr.status === 200) {
+                const { storageId } = JSON.parse(xhr.responseText);
+                await createFiles({ name: fileName, orgId, fileId: storageId, type: fileInputRef.current!.files![0].type as FileType });
+                onClose();
+                setIsLoading(false);
+                setFileName('');
+                setUploadProgress(0);
+                setError(null);
+                if (fileInputRef.current) {
+                    fileInputRef.current.value = '';
+                }
+
+                toast({
+                    title: "File uploaded successfully",
+                    status: "success",
+                    duration: 2000,
+                    isClosable: true,
+                    position: "bottom-right"
+                });
+            } else {
+                setError("Failed to upload file");
+                toast({
+                    title: "Failed to upload file",
+                    status: "error",
+                    duration: 2000,
+                    isClosable: true,
+                    position: "bottom-right"
+                });
+            }
+        };
+
+        xhr.onerror = () => {
             setError("Failed to upload file");
             toast({
                 title: "Failed to upload file",
@@ -108,12 +129,36 @@ const UploadButton = () => {
                 isClosable: true,
                 position: "bottom-right"
             });
-        }
+        };
+
+        xhr.send(fileInputRef.current!.files![0]);
     };
 
     return (
         <>
-            <Button onClick={onOpen}>Upload File</Button>
+            <Tooltip label="Upload" placement="top">
+                <Button
+                    onClick={onOpen}
+                    position="fixed"
+                    bottom={["30px", "50px"]}
+                    right={["30px", "50px"]}
+                    zIndex="sticky"
+                    boxShadow="lg"
+                    aria-label="Upload File"
+                    borderRadius="full"
+                    width={["60px", "75px"]}
+                    height={["60px", "75px"]}
+                    bgGradient="linear(to-br, blue.500, green.400, yellow.300)"
+                    _hover={{
+                        bgGradient: "linear(to-br, blue.600, green.500, yellow.400)"
+                    }}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width={30} height={30} viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+                        <line x1="12" y1="5" x2="12" y2="19"></line>
+                        <line x1="5" y1="12" x2="19" y2="12"></line>
+                    </svg>
+                </Button>
+            </Tooltip>
 
             <Modal isOpen={isOpen} onClose={onClose}>
                 <ModalOverlay />
@@ -154,18 +199,28 @@ const UploadButton = () => {
                                 {error}
                             </Text>
                         )}
+                        
                     </ModalBody>
 
                     <ModalFooter>
-                        <Button
-                            colorScheme="blue"
-                            mr={3}
-                            onClick={handleSubmit}
-                            isLoading={isLoading}
-                            disabled={isLoading || !isFormValid}
-                        >
-                            Submit
-                        </Button>
+                    
+                            <Flex alignItems="center" justifyContent="flex-end" mt={2}>
+                                <Button
+                                    colorScheme="blue"
+                                    mr={3}
+                                    onClick={handleSubmit}
+                                    isLoading={isLoading}
+                                    disabled={isLoading || !isFormValid}
+                                >
+                                    Submit
+                                </Button>
+                                </Flex>
+                                {uploadProgress > 0 && uploadProgress < 100 && (
+                                <CircularProgress value={uploadProgress} color="blue.400" size="50px" thickness="4px">
+                                    <CircularProgressLabel fontSize="xs">{`${Math.round(uploadProgress)}%`}</CircularProgressLabel>
+                                </CircularProgress>
+                            
+                        )}
                         <Button variant="ghost" onClick={onClose}>Cancel</Button>
                     </ModalFooter>
                 </ModalContent>
